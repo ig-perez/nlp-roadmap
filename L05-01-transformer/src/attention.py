15f107d41e8e667a8c38890a9d9956854360510b
Ivan Perez <dev@ivanperez.pe>
2021-07-26 23:11:57 -0500

    Assignment completed. Todo: Review code and check test evaluation before push

--- L05-01-transformer/src/attention.py
+++ L05-01-transformer/src/attention.py
@@ -105,15 +105,24 @@ class SynthesizerAttention(nn.Module):
         # Linear layer w1 will transform x and output y = xA+b. Since x is a
         # multi. dim tensor, the transformation will be applied only to last 
         # dim of x. This is why when defining nn.Linear we say the input is
-        # a vector of features, and the output is also a feature transformed vector
+        # a vector of features, and the output is also a transformed feature vector
+        # Notice, it is the same (transform x with w1 -> reshape -> ReLU) and
+        # (transform -> ReLU -> reshape)
         relu_input = self.w1(x)  # (b, l, d)
         relu = F.relu(relu_input)  # (b, l, d)
 
         # We need to @ relu output with B of shape (d/h, l), then we need to reshape
         # relu output in a way we need to keep the number of features in the tensor
         # i.e. b*l*h*d/h = b*l*d. Notice x.view(b, l, h, d//h).transpose(1,2) != x.view(b, h, l, d//h)
-        softmax_input = relu.view(b, l, h, d//h).transpose(1,2) @ self.w2 + self.b2  # (b, h, l, l)
-        softmax_input.masked_fill(self.mask[:,:,:l,:l] == 0, -1e10)
+        # Note: For evaluation time it is needed to make w2.unsqueeze(0).unsqueeze(0)[..., :l] to 
+        # shape it as (1, 1, d//h, l) before @ with relu, same happens with the
+        # sum with b2 (with b2[..., :l]) at training time PyTorch can handle with those changes!
+        # Not sure why, perhaps because at eval time l has different size
+        w2 = self.w2.unsqueeze(0).unsqueeze(0)[..., :l]  # (1, 1, d//h, l)
+        b2 = self.b2[..., :l]
+
+        softmax_input = relu.view(b, l, h, d//h).transpose(1,2) @ w2 + b2  # (b, h, l, l)
+        softmax_input = softmax_input.masked_fill(self.mask[:,:,:l,:l] == 0, -1e10)
         softmax = F.softmax(softmax_input, dim=-1)  # (b, h, l, l)
         softmax = self.attn_drop(softmax)  # (b, h, l, l)
 
